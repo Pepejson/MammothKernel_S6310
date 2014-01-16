ebd3c691fe771a43bd4e580ac923d2cd7e0e6bcb
mamutos <pepedziobak@hotmail.com>
2014-01-16 17:39:07 +0100

    Added VR iosched & Minmax Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -423,6 +423,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
+extern struct cpufreq_governor cpufreq_gov_minmax;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_minmax)
 #endif
 
 
