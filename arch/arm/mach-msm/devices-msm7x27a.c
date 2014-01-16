9e3f4095ab940f148fe219273c0da883528fb0ba
mamutos <pepedziobak@hotmail.com>
2014-01-16 16:02:32 +0100

    Battery life tweak for GPU

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -984,11 +984,13 @@ struct platform_device msm_kgsl_3d0 = {
 void __init msm7x25a_kgsl_3d0_init(void)
 {
 	if (cpu_is_msm7x25a() || cpu_is_msm7x27aa() || cpu_is_msm7x25aa() || cpu_is_msm7x25ab()) {
-		kgsl_3d0_pdata.num_levels = 2;
+		kgsl_3d0_pdata.num_levels = 3;
 		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 192000000;
 		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 192000000;
 		kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 133330000;
 		kgsl_3d0_pdata.pwrlevel[1].bus_freq = 160000000;
+		kgsl_3d0_pdata.pwrlevel[2].gpu_freq = 96000000;
+		kgsl_3d0_pdata.pwrlevel[2].bus_freq = 0;
 	}
 }
 
