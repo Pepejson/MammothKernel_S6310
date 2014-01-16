deffe0af52bdb335e341af0c6f2256a2720ab4d4
mamutos <pepedziobak@hotmail.com>
2014-01-16 17:16:17 +0100

    Removed interactiveX(Will be added in future, need to fix)

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Thu Jan 16 15:56:03 CET 2014"
+#define UTS_VERSION "#14 SMP PREEMPT Thu Jan 16 17:08:02 CET 2014"
 #define LINUX_COMPILE_BY "pepejson"
 #define LINUX_COMPILE_HOST "pepejson-build"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0MammothKernel-g6e19726-dirty"
+#define UTS_RELEASE "3.4.0MammothKernel-g25568ba-dirty"
