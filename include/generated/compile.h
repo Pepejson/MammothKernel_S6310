28c63d1afee4f852aa73c37ad59254a88c1d6829
mamutos <pepedziobak@hotmail.com>
2014-01-16 16:10:27 +0100

    Deleted defconfs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Thu Jan 16 15:34:48 CET 2014"
+#define UTS_VERSION "#12 SMP PREEMPT Thu Jan 16 15:56:03 CET 2014"
 #define LINUX_COMPILE_BY "pepejson"
 #define LINUX_COMPILE_HOST "pepejson-build"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-g50fa326-dirty"
+#define UTS_RELEASE "3.4.0-g67f3cc9-dirty"
