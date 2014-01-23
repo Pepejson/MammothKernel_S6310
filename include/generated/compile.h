3c3d31dac36fc00c09e120eb0c76e1dfc75b5f0f
mamutos <pepedziobak@hotmail.com>
2014-01-23 20:10:12 +0100

    Removed old GPU Overlock and working on new

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 68 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#15 SMP PREEMPT Thu Jan 16 17:55:25 CET 2014"
+#define UTS_VERSION "#68 SMP PREEMPT Thu Jan 23 20:04:21 CET 2014"
 #define LINUX_COMPILE_BY "pepejson"
 #define LINUX_COMPILE_HOST "pepejson-build"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-MammothKernel-gebd3c69-dirty"
+#define UTS_RELEASE "3.4.0-MammothKernel-g6c02cf8-dirty"
